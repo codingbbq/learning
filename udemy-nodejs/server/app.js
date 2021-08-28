6b3e3d6f166775531702afd47932e403d9e3dc21
codingbbq <codingbbq@gmail.com>
2021-08-28 14:28:26 +0530

    Lesson : Building a HTTP web server

--- udemy-nodejs/server/app.js
+++ udemy-nodejs/server/app.js
@@ -6,7 +6,13 @@ http.createServer(function(req, res){
         'Content-Type': 'text/html'
     });
 
+    // Read the file and then pass the data to the response.
+    /*
     var html = fs.readFileSync(__dirname + '/index.html');
     res.end(html);
+    */
+
+    // Read the file stream and then pass it via pipe
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
 
 }).listen(1337, '127.0.0.1');
